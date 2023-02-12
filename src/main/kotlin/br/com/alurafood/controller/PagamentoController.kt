4ed91fa820c0f10cbb1fe034ae11d2a4438e9341
Bernardo Porto e Silva Souza <bernardo.souza@cwi.com.br>
2023-02-11 22:31:39 -0300

    removendo imports não usados

--- src/main/kotlin/br/com/alurafood/controller/PagamentoController.kt
+++ src/main/kotlin/br/com/alurafood/controller/PagamentoController.kt
@@ -2,22 +2,12 @@ package br.com.alurafood.controller
 
 import br.com.alurafood.dto.PagamentoDTO
 import br.com.alurafood.service.CrudPagamentoService
-import jakarta.validation.Valid
-import org.apache.coyote.Response
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/pagamentos")
